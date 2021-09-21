02a1a5f1aa4567505cf5cb5fcd5bb3989d39a160
Kobe Robinson <koberobinson02@gmail.com>
2021-09-21 21:57:25 +1000

    no idea

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword()  { 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
