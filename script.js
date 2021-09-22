b1d1a9d534d6fa2b1b21be17c622df057d2a2c1a
Kobe Robinson <koberobinson02@gmail.com>
2021-09-22 15:51:13 +1000

    adding all of the potential characters that could be inside of the password

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
 
+specialCharacters = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "U", "R", "S", "T", "U", "V", "W", "X", "y", "Z"];
+lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
 
 // Write password to the #password input
 function writePassword()  { 
