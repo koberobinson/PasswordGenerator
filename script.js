edf1859bfe6aebf9032f302d885c5e82e95adc42
Kobe Robinson <koberobinson02@gmail.com>
2021-09-22 22:22:13 +1000

    adding all the else if for if two of the options are chose

--- script.js
+++ script.js
@@ -36,23 +36,41 @@ function writePassword() {
 
   if (!confirmSpecialCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
     choices = alert("You must choose atleast 1 of the criteria.")
-  } 
+  }
   else if (confirmSpecialCharacter && confirmNumber && confirmUppercase) {
-      choices = specialCharacters.concat(specialCharacters, numbers, upperCase);
+    choices = specialCharacters.concat(specialCharacters, numbers, upperCase);
   }
   else if (confirmSpecialCharacter && confirmNumber && confirmUppercase) {
     choices = character.concat(numbers, upperCase);
-}
+  }
   else if (confirmSpecialCharacter && confirmNumber && confirmLowercase) {
     choices = character.concat(numbers, lowerCase);
-}
+  }
   else if (confirmSpecialCharacter && confirmLowercase && confirmUppercase) {
     choices = character.concat(lowerCase, upperCase);
-}
+  }
   else if (confirmNumber && confirmLowercase && confirmUppercase) {
     choices = number.concat(lowerCase, upperCase);
-}
-  
+  }
+  else if (confirmCharacter && confirmNumber) {
+    choices = character.concat(number);
+  } 
+  else if (confirmCharacter && confirmLowercase) {
+    choices = character.concat(alpha);
+  } 
+  else if (confirmCharacter && confirmUppercase) {
+    choices = character.concat(alpha2);
+  }
+  else if (confirmLowercase && confirmNumber) {
+    choices = alpha.concat(number);
+  } 
+  else if (confirmLowercase && confirmUppercase) {
+    choices = alpha.concat(alpha2);
+  } 
+  else if (confirmNumber && confirmUppercase) {
+    choices = number.concat(alpha2);
+  }
+
 
 
 
